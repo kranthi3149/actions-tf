66e1a298f8b82131747e2279599e50f8102660b8
kranthi3149 <93141895+kranthi3149@users.noreply.github.com>
2024-12-04 11:21:05 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,12 +1,21 @@
-resource "aws_s3_bucket" "bucket1" {
-	bucket = "kranthi-first-bucket"
+resource "aws_s3_bucket" "bucket3" {
+	bucket = "neeha-first-bucket"
 }
 
-resource "aws_s3_bucket" "bucket2" {
-   bucket = "kranthi-second-bucket" 
+resource "aws_s3_bucket" "bucket4" {
+    
 }
 
 provider "aws" {
     region = "us-east-1"
 }
 
+terraform {
+  backend "s3" {
+    bucket         = "neeha-s3bucket-0412"
+    key            = "neeha/s3/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "neeha-0412-dblocks"
+    encrypt        = true
+  }
+}
